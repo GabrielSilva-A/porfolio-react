233ddc0e94ad196bbcdd192eec11b48bbe458d09
Alan Gabriel Silva <ags0014@gmail.com>
2026-07-22 16:08:42 -0300

    Segundo commit

--- client/vite.config.cjs
+++ client/vite.config.cjs
@@ -2,6 +2,7 @@ const { defineConfig } = require('vite');
 const react = require('@vitejs/plugin-react');
 
 module.exports = defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/porfolio-react/' : '/',
   plugins: [react()],
   root: 'client',
   server: {
